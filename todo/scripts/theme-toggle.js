5357427555ee3c701c972d4c0fc05cac708404c1
muhammad <muhammadkeyton@gmail.com>
2022-02-04 16:35:17 +0300

    few changes in style

--- todo/scripts/theme-toggle.js
+++ todo/scripts/theme-toggle.js
@@ -6,9 +6,15 @@
         return function() {
             if(isDarkTheme) {
                 document.documentElement.setAttribute('data-theme', 'light');
+
+                //changing the background image depending on the current theme
+                document.querySelector(".top-section").classList.remove("top-section-dark-theme");
                 
             } else {
                 document.documentElement.setAttribute('data-theme', 'dark');
+
+                //changing the background image depending on the current theme
+                document.querySelector(".top-section").classList.add("top-section-dark-theme");
             }
             document.querySelector("#light-theme").className = isDarkTheme ? displayNoneCSS : "";
             document.querySelector("#dark-theme").className = isDarkTheme ? "" : displayNoneCSS; 
