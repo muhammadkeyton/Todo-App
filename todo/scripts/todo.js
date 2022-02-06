2205b2e184d8dc8a98d4ef6fefe68c54aad35bed
Jeffer Marcelino <jeffermarcelinodev@gmail.com>
2022-02-06 11:42:51 +0200

    fixing some bugs and start adding a todo to a all-todos

--- todo/scripts/todo.js
+++ todo/scripts/todo.js
@@ -1,7 +1,31 @@
 const task = document.querySelector("input#input")
+const todos = document.querySelector("main .all-todos")
 
 task.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
-      console.log(task.value)
+      if (task.value.length == 0) {
+        alert("Insira um valor!")
+      } else {
+        card = document.createElement("div")
+        cardBody = document.createElement("div")
+        todoItem = document.createElement("div")
+        radio = document.createElement("input")
+        item = document.createElement("p")
+
+        card.setAttribute("class", "card")
+        cardBody.setAttribute("class", "card-body")
+        todoItem.setAttribute("class", "todo-item")
+        radio.setAttribute("type", "radio")
+        item.setAttribute("class", "item")
+
+        item.innerHTML = `${task.value}`
+
+        todoItem.appendChild(radio)
+        todoItem.appendChild(item)
+        cardBody.appendChild(todoItem)
+        card.appendChild(cardBody)
+        todos.appendChild(card)
+      }
+      task.value = ""
     }
 });
